70bd93719518f6df3d0b6fd5b9b8ec492f60d204
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-06-06 23:17:52 -0400

    Removed unnecessary lines - cleaned code

--- Coronavirus.R
+++ Coronavirus.R
@@ -1,3 +1,4 @@
+options(scipen=10)
 devtools::install_github("RamiKrispin/coronavirus")
 
 
@@ -9,8 +10,6 @@ data("coronavirus")
 
 compare_first_days <- coronavirus %>%
   filter(type=="confirmed") %>%
-  #filter(Country.Region%in%c('Chile','Spain','Italy','Germany'),type=="confirmed") %>%
-  #filter(Province.State%in%c("Hubei","")) %>%
   group_by(date,country,type) %>%
   summarise(cases=sum(cases)) %>%
   group_by(country) %>%
@@ -19,13 +18,13 @@ compare_first_days <- coronavirus %>%
   mutate(days_since_start=date-min(date),
          curr_max=max(total_cases)) %>%
   filter(#days_since_start<40, #in case you just want to plot the first days
-         curr_max>40)
+         curr_max>400) #to keep the dataset lighter, remove countries with few cases
 
 compare_first_days %>%
-  filter(country%in%c('Chile','Brazil','Peru','Ecuador','Argentina','Colombia')) %>%
-  #filter(country%in%c('Chile','Germany','Spain','Italy','Brazil','Iran')) %>%
+  #filter(country%in%c('Chile','Brazil','Peru','Ecuador','Argentina','Colombia')) %>%
+  filter(country%in%c('Chile','Germany','Spain','Italy','Brazil','Iran')) %>%
   ggplot(aes(x=days_since_start,y=total_cases,col=country))+
   geom_line(size=0.8)+
   scale_color_manual(values=c("red","green","blue","orange","brown","black"))
-  scale_y_log10()
+  #scale_y_log10()
   #scale_size_manual(values=c(rep(0.5,2),2))
